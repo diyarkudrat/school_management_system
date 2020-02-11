ae9905d13a2986ade729f56eae74e4297c55f583
Diyar Kudrat <dkudrat@gmail.com>
2020-02-11 14:04:47 -0800

    removed unneccessary app

--- assignment/urls.py
+++
@@ -1,6 +0,0 @@
-"""Urls.py"""
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.assignments_view, name='assignment_view'),
-]
--- assignment/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def assignments_view(request):
-    return render(request, 'assignment/assignment.html')
