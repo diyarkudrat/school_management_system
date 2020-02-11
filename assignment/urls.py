18d4203537aad08cf7c40cc61c9263ca8763b05c
John Montejano <johnmontejano@Johns-MacBook-Pro.local>
2020-02-11 12:57:42 -0800

    html templates

--- assignment/urls.py
+++ assignment/urls.py
@@ -1,6 +1,6 @@
 """Urls.py"""
 from django.urls import path
-
+from . import views
 urlpatterns = [
-
+    path('', views.assignments_view, name='assignment_view'),
 ]
--- schooly/urls.py
+++ schooly/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('assignments/', include('assignment.urls'), name="assignment"),
+    path('courses/', include('classes.urls'), name="courses"),
 ]
