abd0d6dca4c02d871a7b587e2e805eae11cb23ff
Diyar Kudrat <dkudrat@gmail.com>
2020-02-11 14:13:39 -0800

    removed conflicting files

--- classes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Session, Semester, Course
-# Register your models here.
-
-admin.site.register(Session)
-admin.site.register(Semester)
-admin.site.register(Course)
