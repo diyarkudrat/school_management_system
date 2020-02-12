ced6ac1002b05c6e874b7d3fc71adc09ae54e6d5
Diyar Kudrat <dkudrat@gmail.com>
2020-02-12 10:34:32 -0800

    fix merge conflict

--- classes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-        path('', views.home, name="home"),
-        path('profile/', views.profile, name="profile"),
-        path('courses/', views.CourseListView.as_view(), name="course-list"),
-
-]
